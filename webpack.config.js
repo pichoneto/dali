c5dc93e7156f5dc53456b895d341aed3821bba0b
berto_abc <bertocode@gmail.com>
2016-06-03 18:03:54 +0200

    several changes not working though

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,11 @@ module.exports = {
           'process.env': {
             'NODE_ENV': '"production"'
           }
-        })
+        }),
+        new webpack.ProvidePlugin({
+            'Promise': 'es6-promise', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        }),
     ],
     output: {
         path: __dirname + '/dist',
