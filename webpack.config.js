44741e0f5bd056e2a4bddd62169c280e6abe0b21
berto_abc <bertocode@gmail.com>
2016-07-21 12:29:08 +0200

    Add Loaders

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,43 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel'
+                loader: 'react-hot-loader!babel-loader?presets[]=es2015,presets[]=react',
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: 'style-loader!css-loader'
             },
             {
                 test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
+                exclude: /node_modules/,
+                loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.(jpg|gif|png)$/,
+                loader: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.(woff2?|svg)$/,
+                loader: 'url-loader?limit=10000' },
+            {
+                test: /\.(ttf|eot)$/,
+                loader: 'file-loader' },/*
+            {
+                test: require.resolve('jquery'),
+                loader: 'expose?jQuery!expose?$!expose?window.jQuery'  //expose-loader, exposes as global variable
+            }*/
+            {
+                test: require.resolve('jszip'),
+                loader: 'expose?JSZip'
+            },
+            {
+                test: require.resolve('jszip-utils'),
+                loader: 'expose?JSZipUtils'
+            },
+            {
+                test: require.resolve('file-saver'),
+                loader: 'expose?FileSaver'
             }
         ]
     },
